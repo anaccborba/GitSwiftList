628615ef30573578668e264413c77bafd8cb7095
ana borba <accamargoborba@gmail.com>
2025-01-17 07:51:55 -0300

    Initial Commit

---
+++ GitSwiftList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitSwiftList
+//
+//  Created by Ana Carolina Camargo Borba on 17/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GitSwiftListUITests/GitSwiftListUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  GitSwiftListUITestsLaunchTests.swift
+//  GitSwiftListUITests
+//
+//  Created by Ana Carolina Camargo Borba on 17/01/25.
+//
+
+import XCTest
+
+final class GitSwiftListUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
